fe945b16beff2e9c36d851a4a5836fc542533e52
moiahm886 <90629660+moiahm886@users.noreply.github.com>
2022-07-10 01:22:48 +0500

    Update Exoplanets.cpp

--- Exoplanets/Exoplanets.cpp
+++ Exoplanets/Exoplanets.cpp
@@ -16,7 +16,7 @@ int main()
 	} while (T < 1 || T>20);
 	while (T--)
 	{
-		long double R, h1, h2;
+		double R, h1, h2;
 		do {
 			cin >> R;
 		} while (R < 100 || R>20000);
@@ -24,13 +24,13 @@ int main()
 			cin >> h1 >> h2;
 		} while (h1 < 1 || h1>1000 || h2 < 1 || h2>1000);
 		R *= 1000;
-		long double L1 = R - h1;
-		long double L2 = R - h2;
-		long double A1 = acos(L1 / R);
-		long double A2 = acos(L2 / R);
-		long double A = A1 + A2;
-		long double distance = A * R / 1000;
-		cout << distance;
+		double L1 = R - h1;
+		double L2 = R - h2;
+		double A1 = acos(L1 / R);
+		double A2 = acos(L2 / R);
+		double A = A1 + A2;
+		double distance = A * R / 1000;
+		cout << distance<<endl;
 	}
 	return 0;
 }
